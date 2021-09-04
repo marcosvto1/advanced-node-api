3872fd39ee7d69dded6bdedbe292deb433a9e2ac
Marcos <marcosvto1@gmail.com>
2021-09-04 20:37:02 -0300

    move business logic to a domain entity

--- src/data/services/facebook-authentication.ts
+++ src/data/services/facebook-authentication.ts
@@ -2,6 +2,7 @@ import { LoadFacebookUserApi } from '@/data/contracts/apis/facebook'
 import { AuthenticationError } from '@/domain/errors'
 import { FacebookAuthentication } from '@/domain/features'
 import { UserAccountRepository } from '@/data/contracts/repo'
+import { FacebookAccount } from '@/domain/models/facebook-account'
 
 export class FacebookAuthenticationService {
   constructor (
@@ -12,15 +13,11 @@ export class FacebookAuthenticationService {
   async perform (params: FacebookAuthentication.Params): Promise<AuthenticationError> {
     const fbData = await this.facebookApi.loadUser({ token: params.token })
     if (fbData !== undefined) {
-      const userAccount = await this.userAccountRepo.load({
+      const accountFounded = await this.userAccountRepo.load({
         email: fbData.email
       })
-      await this.userAccountRepo.saveWithFacebook({
-        id: userAccount?.id,
-        name: userAccount?.name ?? fbData.name,
-        email: fbData.email,
-        facebookId: fbData.facebookId
-      })
+      const facebookAccount = new FacebookAccount(fbData, accountFounded)
+      await this.userAccountRepo.saveWithFacebook(facebookAccount)
     }
     return new AuthenticationError()
   }

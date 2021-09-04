cff26df4efc53a08d15e364d36f751a4e7cc0425
Marcos <marcosvto1@gmail.com>
2021-09-03 21:03:58 -0300

    add jest

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '@/tests/(.+)': '<rootDir>/tests/$1',
+    '@/(.+)': '<rootDir>/src/$1'
+  },
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/tests'
+  ],
+  transform: {
+    '\\.ts': 'ts-jest'
+  }
+}

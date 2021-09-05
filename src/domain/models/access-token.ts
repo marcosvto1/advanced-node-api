52ce3c22afaccddd984e7986eaadc4ab0951e034
Marcos <marcosvto1@gmail.com>
2021-09-05 14:27:47 -0300

    ensure AccessToken expire in 30 minutes

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,3 +1,14 @@
+
+enum Time {
+  MIN=30,
+  SECOND=60,
+  MILLI=1000
+}
+
 export class AccessToken {
   constructor (private readonly value: string) {}
+
+  static get expirationInMs (): number {
+    return Time.MIN * Time.SECOND * Time.MILLI
+  }
 }

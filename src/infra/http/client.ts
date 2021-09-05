d90af9081d81c89a92a29ca696f79515854c3652
Marcos <marcosvto1@gmail.com>
2021-09-05 20:14:46 -0300

    ensure FacebookApi gets debug token

--- src/infra/http/client.ts
+++ src/infra/http/client.ts
@@ -1,15 +1,13 @@
 
 export interface HttpGetClient {
-  get: (params: HttpGetClient.Params) => Promise<void>
+  get: (params: HttpGetClient.Params) => Promise<HttpGetClient.Result>
 }
 
 export namespace HttpGetClient {
   export type Params = {
     url: string
-    params: {
-      client_id: string
-      client_secret: string
-      grant_type: string
-    }
+    params: any
   }
+
+  export type Result = any
 }
--- tests/infra/apis/facebook.spec.ts
+++ tests/infra/apis/facebook.spec.ts
@@ -1,11 +1,11 @@
 import { FacebookApi } from '@/infra/apis'
 import { HttpGetClient } from '@/infra/http'
-import { mock } from 'jest-mock-extended'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('FacebookApi', () => {
   const clientId = 'any_client_id'
   const clientSecret = 'any_client_secret'
-  let httpClient: HttpGetClient
+  let httpClient: MockProxy<HttpGetClient>
   let sut: FacebookApi
 
   beforeAll(() => {
@@ -13,6 +13,9 @@ describe('FacebookApi', () => {
   })
 
   beforeEach(() => {
+    httpClient.get.mockResolvedValueOnce({
+      access_token: 'any_app_token'
+    })
     sut = new FacebookApi(httpClient, clientId, clientSecret)
   })
 
@@ -25,6 +28,18 @@ describe('FacebookApi', () => {
         client_id: clientId,
         client_secret: clientSecret,
         grant_type: 'client_crendentials'
+      }
+    })
+  })
+
+  it('should get app token', async () => {
+    await sut.loadUser({ token: 'any_client_token' })
+
+    expect(httpClient.get).toHaveBeenCalledWith({
+      url: 'https://graph.facebook.com/debug_token',
+      params: {
+        access_token: 'any_app_token',
+        input_token: 'any_client_token'
       }
     })
   })

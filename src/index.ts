645dc90101b23fca2e3f727facc222272bd89c13
Marcos <marcosvto1@gmail.com>
2021-09-07 19:39:45 -0300

    ensure PgUserAccountRepository loads account if email exists

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-class Person {
-  speak (name?: string): string {
-    return `Olá ${name?.toUpperCase() ?? 'Foo'}!!!`
-  }
-}
-
-const p = new Person()
-p.speak()
-p.speak()
+import 'reflect-metadata'

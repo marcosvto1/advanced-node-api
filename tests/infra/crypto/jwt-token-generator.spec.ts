157a2515140275105a866c98a8823c3a1a8790cf
Marcos <marcosvto1@gmail.com>
2021-09-07 18:42:30 -0300

    isolate SUT creation

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -7,7 +7,6 @@ class JwtTokenGenerator {
   ) {}
 
   async generateToken (params: TokenGenerator.Params): Promise<void> {
-    // await jwsign(params.key, 'any_secret')
     const expirationInSecondes = params.expirationInMs / 1000
     jwt.sign({
       key: params.key
@@ -23,10 +22,18 @@ class JwtTokenGenerator {
 jest.mock('jsonwebtoken')
 
 describe('JwtTokenGenerator', () => {
-  it('should call sign with correct params', async () => {
-    const fakeJwt = jwt as jest.Mocked<typeof jwt>
-    const sut = new JwtTokenGenerator('any_secret')
+  let sut: JwtTokenGenerator
+  let fakeJwt: jest.Mocked<typeof jwt>
 
+  beforeAll(() => {
+    fakeJwt = jwt as jest.Mocked<typeof jwt>
+  })
+
+  beforeEach(() => {
+    sut = new JwtTokenGenerator('any_secret')
+  })
+
+  it('should call sign with correct params', async () => {
     await sut.generateToken({
       key: 'any_key',
       expirationInMs: 1000

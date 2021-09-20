9fca35aae8117de39247ffa9c17ecd2df54645ad
Marcos <marcosvto1@gmail.com>
2021-09-20 20:04:21 -0300

    ensure RequiredStringValidator returns Error if value is empty

---
+++ tests/application/validation/required-string.spec.ts
@@ -0,0 +1,47 @@
+import { HttpError } from '@/application/helpers'
+
+class RequiredStringValidator {
+  constructor (private readonly value: any, private readonly field: string) {}
+
+  validate (): Error | true {
+    if (this.value === '' || this.value === null || this.value === undefined) {
+      return new HttpError.RequiredField('any_field')
+    }
+
+    return true
+  }
+}
+
+describe('RequiredStringValidator', () => {
+  it('should return Error if value is Empty ', () => {
+    const sut = new RequiredStringValidator('', 'any_field')
+
+    const validate = sut.validate()
+
+    expect(validate).toEqual(new HttpError.RequiredField('any_field'))
+  })
+
+  it('should return Error if value is null ', () => {
+    const sut = new RequiredStringValidator(null, 'any_field')
+
+    const validate = sut.validate()
+
+    expect(validate).toEqual(new HttpError.RequiredField('any_field'))
+  })
+
+  it('should return Error if value is undefined ', () => {
+    const sut = new RequiredStringValidator(undefined, 'any_field')
+
+    const validate = sut.validate()
+
+    expect(validate).toEqual(new HttpError.RequiredField('any_field'))
+  })
+
+  it('should reutrn true if value is not Empty ', () => {
+    const sut = new RequiredStringValidator('any+value', 'any_field')
+
+    const validate = sut.validate()
+
+    expect(validate).toBe(true)
+  })
+})

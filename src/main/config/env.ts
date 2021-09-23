6023b9a23536dfe841639013fa33558b14cbee69
Marcos <marcosvto1@gmail.com>
2021-09-23 20:30:55 -0300

    add jest config to integration tests

---
+++ src/main/config/env.ts
@@ -0,0 +1,6 @@
+export const env = {
+  facebookApi: {
+    clientId: process.env.FACEBOOK_CLIENT_ID ?? '454886045787381',
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET ?? '93b95d2b7007ba4d896f3120bb054d37'
+  }
+}
--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,4 +1,22 @@
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
+
 describe('', () => {
-  it('', () => {
+  it('Should return a Facebook if Token is Valid', async () => {
+    const axiosClient = new AxiosHttpClient()
+    const sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+
+    const fbUser = await sut.loadUser({ token: 'EAAGdt2ksJPUBAMZCU4SpfnBoS7svrHvvBblsXIGr9ORoZCZADOa8CsKBsM9YBuFLu1ORZBRtbqwxb3PspAit5lZB32aL6jRBQahz2uuAZAjGPxget68ZCE0K7qjD6jwzrSNHkngZCVlnZAqrciBVEKpNPBvxlgnlQ8F28xSi3AGHlgS76u7M3OnNYAnxIbh5iLdelJz34JcAW8gZDZD' })
+
+    expect(fbUser).toEqual({
+      facebookId: '107921788317232',
+      email: 'marcos_ejuttrh_test@tfbnw.net',
+      name: 'Marcos Test'
+    })
   })
 })

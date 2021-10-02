4156f16a6ba2fba67018d376635f8b9911d1505e
Marcos <marcosvto1@gmail.com>
2021-10-02 16:38:00 -0300

    add postgres env

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 export const env = {
   facebookApi: {
     clientId: process.env.FACEBOOK_CLIENT_ID ?? '454886045787381',
@@ -6,5 +9,12 @@ export const env = {
   app: {
     port: process.env.PORT ?? 8000
   },
-  jwtSecret: process.env.JWT_SECRET ?? 'SADSADSA5546456'
+  jwtSecret: process.env.JWT_SECRET ?? 'SADSADSA5546456',
+  postgres: {
+    host: process.env.DB_HOST ?? 'localhost',
+    port: process.env.DB_PORT ?? 5432,
+    username: process.env.DB_USERNAME ?? 'myuserdb',
+    database: process.env.DB_NAME ?? 'mzbjyekm',
+    password: process.env.DB_PASSWORD ?? 'myuserpassword'
+  }
 }

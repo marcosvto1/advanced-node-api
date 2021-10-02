16471b1baecfab5b9a4ae774585c04ff60b0e83a
Marcos <marcosvto1@gmail.com>
2021-10-02 16:39:39 -0300

    change connection option to use env postgres

--- src/infra/postgres/helpers/config.ts
+++ src/infra/postgres/helpers/config.ts
@@ -1,11 +1,12 @@
+import { env } from '@/main/config/env'
 import { ConnectionOptions } from 'typeorm'
 
 export const config: ConnectionOptions = {
   type: 'postgres',
-  host: 'raja.db.elephantsql.com',
-  port: 5432,
-  username: 'mzbjyekm',
-  database: 'mzbjyekm',
-  password: 'whpRb3T_h0CSbiUS2Soa_XXvw25DelwT',
+  host: env.postgres.host,
+  port: +env.postgres.port,
+  username: env.postgres.username,
+  database: env.postgres.database,
+  password: env.postgres.password,
   entities: ['dist/infra/postgres/entities/index.js']
 }

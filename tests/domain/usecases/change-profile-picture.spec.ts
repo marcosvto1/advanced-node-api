17038fa733e04be5095cfbd1a95c498752f94acc
Marcos <marcosvto1@gmail.com>
2021-10-15 20:43:26 -0300

    ensure ChangeProfilePicture calls UploadFile with correct params

---
+++ tests/domain/usecases/change-profile-picture.spec.ts
@@ -0,0 +1,40 @@
+import { mock } from 'jest-mock-extended'
+
+type Setup = (fileStorage: UploadFile) => ChangeProfilePicture
+type Input = { id: string, file: Buffer}
+type ChangeProfilePicture = (input: Input) => Promise<void>
+const setupChangeProfilePicture: Setup = (fileStorage) => async ({ id, file }) => {
+  await fileStorage.upload({
+    file,
+    key: 'any_id'
+  })
+}
+
+export interface UploadFile {
+  upload: (input: UploadFile.Input) => Promise<void>
+}
+
+namespace UploadFile {
+  export type Input = {
+    file: Buffer
+    key: string
+  }
+}
+
+describe('ChangeProfilePicture', () => {
+  fit('should call UploadFile with correct input', async () => {
+    const file = Buffer.from('any_buffer')
+    const fileStorage = mock<UploadFile>()
+    const sut = setupChangeProfilePicture(fileStorage)
+
+    await sut({
+      id: 'any_id',
+      file
+    })
+
+    expect(fileStorage.upload).toHaveBeenLastCalledWith({
+      file, key: 'any_id'
+    })
+    expect(fileStorage.upload).toHaveBeenCalledTimes(1)
+  })
+})

9b6b8edf74fb51cdea0ef5edc5e050f1b8ab96f6
Marcos <marcosvto1@gmail.com>
2021-10-27 20:02:46 -0300

    ensure change profile picture returns correct data on success

--- src/domain/usecases/change-profile-picture.ts
+++ src/domain/usecases/change-profile-picture.ts
@@ -4,25 +4,16 @@ import { UserProfile } from '@/domain/entities'
 
 type Setup = (fileStorage: UploadFile, generatorUUID: UUIDGenrator, userProfileRepo: SaveUserPicture & LoadUserProfile) => ChangeProfilePicture
 type Input = { id: string, file?: Buffer}
-export type ChangeProfilePicture = (input: Input) => Promise<void>
+type Output = { pictureUrl?: string, initials?: string }
+export type ChangeProfilePicture = (input: Input) => Promise<Output>
 export const setupChangeProfilePicture: Setup = (fileStorage, generatorUUID, userProfileRepo) => async ({ id, file }) => {
-  const uuid = generatorUUID.generate()
-  const data: { pictureUrl?: string, name?: string } = {}
-  let pictureUrl: string | undefined
-  let name: string | undefined
-  if (file !== undefined) {
-    data.pictureUrl = await fileStorage.upload({
-      file,
-      key: uuid
-    })
-  } else {
-    const result = await userProfileRepo.load({
-      id
-    })
-    data.name = result.name
+  const Keyuuid = generatorUUID.generate()
+  const data = {
+    pictureUrl: file !== undefined ? await fileStorage.upload({ file, key: Keyuuid }) : undefined,
+    name: file === undefined ? (await userProfileRepo.load({ id })).name : undefined
   }
   const userProfile = new UserProfile(id)
-  userProfile.setPicture({ pictureUrl, name })
-
+  userProfile.setPicture(data)
   await userProfileRepo.savePicture(userProfile)
+  return userProfile
 }

50375e56e4f7e78212dad9026bbc352638bb21a4
Marcos <marcosvto1@gmail.com>
2021-11-07 19:55:06 -0300

    ensure PgUserProfileRepository returns undefined if id does not exists

--- tests/infra/repos/postgres/repos/user-profile.spec.ts
+++ tests/infra/repos/postgres/repos/user-profile.spec.ts
@@ -58,5 +58,11 @@ describe('PgUserProfileRepository', () => {
 
       expect(userProfile?.name).toBe('any_name')
     })
+
+    it('should return undefined if not found user', async () => {
+      const userProfile = await sut.load({ id: '1' })
+
+      expect(userProfile).toBeUndefined()
+    })
   })
 })

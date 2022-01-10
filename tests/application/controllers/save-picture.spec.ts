4cfc8decf8a8384819f2ab1c39f1eaf856117720
Marcos <marcosvto1@gmail.com>
2022-01-10 19:45:06 -0300

    isolate sut creation

--- tests/application/controllers/save-picture.spec.ts
+++ tests/application/controllers/save-picture.spec.ts
@@ -10,10 +10,23 @@ export class SavePictureController {
 }
 
 describe('SavePictureController', () => {
-  it('should returns 400 if file is not provided', async () => {
-    const sut = new SavePictureController()
+  let sut: SavePictureController
 
+  beforeEach(() => {
+    sut = new SavePictureController()
+  })
+
+  it('should returns 400 if file is not provided', async () => {
     const httpResponse = await sut.perform({ file: undefined })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new HttpError.RequiredField('file')
+    })
+  })
+
+  it('should returns 400 if file is not provided', async () => {
+    const httpResponse = await sut.perform({ file: null })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
